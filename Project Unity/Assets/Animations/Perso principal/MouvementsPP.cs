2fd039402d25d32c58409ddb576ab733e53caa83
Xdarwin <guidrapbiz@hotmail.fr>
2015-03-07 18:00:29 +0100

    Animation avec le script
    Je dois encore modifier le perso pour ajouter des scripts et pour qu'il
    tourne sur lui même.

--- Project Unity/Assets/Animations/Perso principal/MouvementsPP.cs
+++ Project Unity/Assets/Animations/Perso principal/MouvementsPP.cs
@@ -20,7 +20,7 @@ public class MouvementsPP : MonoBehaviour {
 	void Start () {
         controller = GetComponent<CharacterController>();
         moveDirection = new Vector3(0f, 0f, 0f);
-        characterContent = transform.Find("Perso Principal FInal");
+        characterContent = transform.Find("Perso Principal FInal 1");
 	}
 
 
@@ -32,11 +32,12 @@ public class MouvementsPP : MonoBehaviour {
 
         if(controller.isGrounded)
         {
+            //On prend les différentes valeurs des axes horizontaux et verticaux et on les met dans le moveDirection (vecteur du déplacement du perso)
             moveDirection.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection *= speed;
 
 
-            if (Input.GetKey("left shift"))
+            if (Input.GetKey("left shift")) //Si le perso court.
             {
                 moveDirection *= speedRun;
                 characterContent.animation.CrossFade("Anim - Courrir", 0.2f);
@@ -44,7 +45,7 @@ public class MouvementsPP : MonoBehaviour {
             }
             else
             {
-                if (Input.GetButton("Jump"))
+                if (Input.GetButton("Jump")) //Si le perso saute
                 {
                     characterContent.animation.CrossFade("Animation - Saut", 0.2f);
                     WaitForAnimation();
@@ -52,7 +53,7 @@ public class MouvementsPP : MonoBehaviour {
                 }
                 else
                 {
-                    characterContent.animation.CrossFade("Anim - Marche");
+                    characterContent.animation.CrossFade("Anim - Marche"); //Si il ne court ni ne saute et qu'il bouge, alors il marche
                 }
             }
 
@@ -62,12 +63,15 @@ public class MouvementsPP : MonoBehaviour {
                 characterContent.animation.CrossFade("Anim - Idle", 0.2f);
         }
 
-        moveDirection.y -= gravity * deltaTime;
+        //Gravity et on bouge le character controller
+        moveDirection.y -= gravity * deltaTime; 
         controller.Move(moveDirection * deltaTime);
 
 	}
 
 
+    //Fonction qui sert à attendre une seconde, soit le temps d'appliquer l'animation. On pourra la changer plus tard avec en paramètres le nombre de secondes
+    //si on veut réaliser une animation plus longue.
     private IEnumerator WaitForAnimation()
     {
         yield return new WaitForSeconds(1);
