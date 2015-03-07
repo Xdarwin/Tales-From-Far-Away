647223b6ad38ee0ec895a22be168a9e81c48e19f
Xdarwin <guidrapbiz@hotmail.fr>
2015-03-07 16:08:27 +0100

    On change tout !

---
+++ Project Unity/Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	private Vector3 to;
+	private Vector3 mouseDirection;
+	float sensiverticale = (float)150.0;
+	Vector3 angle;
+	private float x = (float)0.0;
+	private float y = (float)0.0;
+	
+	// Use this for initialization
+	void Start () {
+		angle = transform.eulerAngles;
+		x = angle.y;
+		y = angle.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			x += Input.GetAxis("Mouse X") * sensiverticale * (float)0.02;
+			Quaternion rotation = Quaternion.Euler(y, x, 0);
+			transform.rotation = rotation;
+	}
+
+	
+	static float ClampAngle (float angle, float min, float max) {
+		if (angle < -360)
+			angle += 360;
+		if (angle > 360)
+			angle -= 360;
+		return Mathf.Clamp (angle, min, max);
+	}
+}
---
+++ Project Unity/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+	public float speed = 6f;            // The speed that the player will move at.
+	Vector3 movement;                   // The vector to store the direction of the player's movement.
+	Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+	float gravity = (float)1;
+	void Awake ()
+	{
+		// Create a layer mask for the floor layer.
+		
+		// Set up references.
+		playerRigidbody = GetComponent <Rigidbody> ();
+	}
+	
+	
+	void FixedUpdate ()
+	{
+		// Store the input axes.
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+
+		// Move the player around the scene.
+		Move (h, v);
+	}
+	
+	void Move (float h, float v)
+	{
+		// Set the movement vector based on the axis input.
+		movement.Set (h, - gravity, v);
+
+		// Normalise the movement vector and make it proportional to the speed per second.
+		movement = movement.normalized * speed * Time.deltaTime;
+		// Move the player to it's current position plus the movement.
+		playerRigidbody.MovePosition (movement + transform.localPosition);
+
+	}
+}
